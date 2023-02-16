ae02861f54f1138cb68ae26a5b1fe472db2857ea
Michael <michaelelsa12@gmail.com>
2023-02-16 14:08:55 -0800

    Quotec

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main -  A program that print line  of standard eror
+ * Return: 1 (Succes)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
