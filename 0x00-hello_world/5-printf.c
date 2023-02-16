78e4ad9a0020324f8dcd976bb3e963e45c1582fd
Michael <michaelelsa12@gmail.com>
2023-02-16 13:24:26 -0800

    PrintfC

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -  A program that print line  with printf function
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+printf("\n with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
