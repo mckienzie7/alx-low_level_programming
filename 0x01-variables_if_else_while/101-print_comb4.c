205c4227f9aa1b14a19c9f8e834d4c4f02e8e483
Michael <michaelelsa12@gmail.com>
2023-02-17 15:56:30 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,7 +20,7 @@ int main(void)
 			c = 0;
 			while (c < 10)
 			{
-				if (c != d && d != e && e != d && d != c)
+				if (c != d && d != e && e < d && d < c)
 				{
 					putchar('0' + e);
 					putchar('0' + d);
