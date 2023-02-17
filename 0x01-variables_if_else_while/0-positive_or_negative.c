e5b697b1aa5434b3c50137828c8c4b6f3d85a72e
Michael <michaelelsa12@gmail.com>
2023-02-17 12:10:21 -0800

    variable_for_if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive\n", n);
 else if (n == 0)
-printf("%d is zer\no", n);
+printf("%d is zero\n", n);
 else
 printf("%d is negative\n", n);
 return (0);
