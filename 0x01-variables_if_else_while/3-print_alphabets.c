d28eae005d8ad0c326b7e3f1d385294f9697b3cd
Michael <michaelelsa12@gmail.com>
2023-02-17 13:32:33 -0800

    print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+char c;
+char d;
+c = 'a';
+d = 'A';
+while
+	(c <= 'z') {
+		putchar(c);
+		c++;
+}
+putchar('\n');
+while
+(d <= 'Z') {
+	putchar(c);
+	d++;
+}
+return (0);
+}
