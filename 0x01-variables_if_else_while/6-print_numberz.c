a7f932dee8f9f13c029c49278b7beab69610aa45
Michael <michaelelsa12@gmail.com>
2023-02-17 14:24:36 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+int c;
+c = 0;
+while
+	(c < 10) {
+		putchar(c);
+		c++;
+}
+putchar('\n');
+return (0);
+}
