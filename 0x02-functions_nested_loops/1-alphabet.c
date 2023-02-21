e0d54bdfeb692b88dea26c4e5aa313bbeeb8814e
Michael <michaelelsa12@gmail.com>
2023-02-21 01:25:22 -0800

    alphabetc

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * void - Posses the lower aphabet case 
+ *
+ * return -  Always 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
