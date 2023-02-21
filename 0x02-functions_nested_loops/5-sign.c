dc26c8a1222b561816b8cd03136303bb53fa43ee
Michael <michaelelsa12@gmail.com>
2023-02-21 11:58:24 -0800

    signc

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - check code
+ * @n: sign
+ * Return: 1 if it's sign mentioned or 0 anything else
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
+
