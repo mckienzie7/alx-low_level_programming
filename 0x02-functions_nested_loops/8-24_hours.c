e714cdbdf2ef2067157c4b44ba2f94c99df8638f
Michael <michaelelsa12@gmail.com>
2023-02-21 16:30:16 -0800

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bawer - something
+ * jack_bauer - something
  * Return: ...
  */
 
@@ -13,7 +13,7 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			if ((a <= 1 && b <= 9)|| (a <= 2 && a <= 3))
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
 				for (c = 0; c <= 5; c++)
 				{
