03505258e975550d723bd595b33b7f8338d99cd7
Michael <michaelelsa12@gmail.com>
2023-02-23 11:47:01 -0800

    d_main_c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
+	while (i < 10) /* i is not incremented */
 	{
-		putchar(i); /* i is not incremented */
+		putchar(i);
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
