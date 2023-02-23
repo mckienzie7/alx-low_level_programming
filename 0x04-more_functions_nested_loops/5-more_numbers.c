5cb32fead4381ffc7e4aba06d25b48155d77a0c6
Michael <michaelelsa12@gmail.com>
2023-02-23 14:36:58 -0800

    more mum

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+/**
+ * more_numbers - check code
+ * Return: 10 times
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}#include "main.h"
+
+/**
+ * more_numbers - check code
+ * Return: 10 times
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}#include "main.h"
+
+/**
+ * more_numbers - check code
+ * Return: 10 times
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
