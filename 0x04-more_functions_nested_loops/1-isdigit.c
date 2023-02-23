c709ca281a3542f8894554a1c59f20a9abd537ab
Michael <michaelelsa12@gmail.com>
2023-02-23 14:05:36 -0800

    isigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - check code
+ * @x: character
+ * Return: 1 or 0
+ */
+int _isdigit(int x)
+{
+        if (x >= 48 && x <= 57)
+        {
+                return (1);
+        }
+        return (0);
+}
