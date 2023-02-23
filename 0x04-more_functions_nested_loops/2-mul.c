0e338e2fd3681e8d016e97ccaa72ac1e0d7c0b51
Michael <michaelelsa12@gmail.com>
2023-02-23 14:13:17 -0800

    isigit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isdigit - check code
+ * mul - check code
  * @x: character
  * @y: second character
  * Return: The result
  */
-int _mul(int x, int y)
+int mul(int x, int y)
 {
 	return (x * y);
 }
