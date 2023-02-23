322af0a45d878eea05f56cc4ad50333afb849d90
Michael <michaelelsa12@gmail.com>
2023-02-23 13:43:11 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check code
+ * @x: character
+ * Return: 1 or 0
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
