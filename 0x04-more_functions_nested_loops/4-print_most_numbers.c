e5d7a4df6424fc2f7d56e53666f07fcede2b91d9
Michael <michaelelsa12@gmail.com>
2023-02-23 14:28:26 -0800

    most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check code
+ * Return: 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
