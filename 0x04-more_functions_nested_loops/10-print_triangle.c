db1d30c44db61e5900f258bd00d96c0779d5925a
Michael <michaelelsa12@gmail.com>
2023-02-23 15:37:23 -0800

    ptriangle10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - check codde
+ * @size - number for tringle
+ * Return: empty
+ */
+void print_triangle()
+{
+	int x, y, z;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
+			for (z = 0; z <= x; z++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
