e8d53ba465b6f7fc9d65d5e6b8b253e22bc20788
Michael <michaelelsa12@gmail.com>
2023-02-23 16:09:18 -0800

    final12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - print integr
+ * @n: number
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
+}
