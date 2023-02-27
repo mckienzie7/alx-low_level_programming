4b11c91799644256e35b3eb0cdb0faaded61e2bd
Michael <michaelelsa12@gmail.com>
2023-02-27 09:33:03 -0800

    swap _int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - change the code by pointer
+ * @a - integer number
+ * @b - number
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
