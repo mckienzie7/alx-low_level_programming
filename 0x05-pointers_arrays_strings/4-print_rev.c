35067b4e9e4e3ed17e215248d669977041cc20a7
Michael <michaelelsa12@gmail.com>
2023-02-27 12:58:50 -0800

    printrev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,10 @@ void print_rev(char *s)
 	int i = 0;
 
 	while (s[i] != '\0')
-	{
 		i++;
-		for (i = i -  1; 1 >= 0; i--)
-		{
-			_putchar(s[i]);
-		}
-	}
+	for (i = i -  1; 1 >= 0; i--)
+		_putchar(s[i]);
+	
 	_putchar('\n');
 }
 
