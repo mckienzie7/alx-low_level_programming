034aa902285a2574a92e967eb489d4e784c14f12
Michael <michaelelsa12@gmail.com>
2023-02-27 13:17:59 -0800

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,11 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 		i++;
-	for (i = i -  1; 1 >= 0; i--)
+	while (i--)
+	{
 		_putchar(s[i]);
-	
+	}
 	_putchar('\n');
 }
-
