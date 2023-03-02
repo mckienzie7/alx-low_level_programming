1e77babae90a7211f93165d23da1ac272ff20075
Michael <michaelelsa12@gmail.com>
2023-03-02 11:51:59 -0800

    reverse_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - check the code
+ * @a: array
+ * @n: integer number
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
