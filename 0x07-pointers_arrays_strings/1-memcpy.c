85766980769338ce9562f527d88f2fa4b7dedac9
Michael <michaelelsa12@gmail.com>
2023-03-06 11:34:38 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _memcpy - manin entry
+ * _memcpy - Entry point
  * @dest: input
  * @src: input
  * @n: input
- * Retun: Always 0
+ * Return: Always 0
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i= 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 
 	return (dest);
