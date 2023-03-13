a17cb5344ae4b03156b5212c381f4c9350a8da4f
Michael <michaelelsa12@gmail.com>
2023-03-13 08:16:49 -0700

    argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,7 +11,7 @@ char *argstostr(int ac, char **av)
 {
 	int i, j, k, size;
 	char *arg;
-	
+
 	size = 0;
 	k = 0;
 	if (ac == 0 || av == NULL)
@@ -41,6 +41,7 @@ char *argstostr(int ac, char **av)
 			j++;
 			k++;
 		}
+		arg[k] = '\n';
 		k++;
 		i++;
 	}
