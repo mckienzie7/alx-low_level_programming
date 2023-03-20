5f64b8f459b823cc2a1d32c3546e747859d79a19
Michael <michaelelsa12@gmail.com>
2023-03-20 10:13:49 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -5,9 +5,10 @@
  * File: 0-object_like_macro.h
  * Description: Header file that defines a macro named
  *              SIZE as an abbreviation for the token 1024.
- *		Headerfile guarded to avoid problem of double inclusion.
+ *              Headerfile guarded to avoid problem of double inclusion.
  */
 
 #define SIZE 1024
 
 #endif
+~       
