be12fbd19d227917db2ecc357619927efffdebbc
Michael <michaelelsa12@gmail.com>
2023-03-23 12:12:55 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 
-
-	/**
+/**
 	 * sum_them_all - Returns the sum of all its paramters.
 	 * @n: The number of paramters passed to the function.
 	 * @...: A variable number of paramters to calculate the sum of.
