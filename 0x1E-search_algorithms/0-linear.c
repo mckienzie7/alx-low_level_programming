581f754771d49b23067026ae93b007a2cdc6e532
root <michaelelsa12@gmail.com>
2023-09-13 05:35:37 -0700

    Linear Search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,15 +10,11 @@
  * Return: value or -1
  */
 
-int linear_search(int *array, size_t size, int value){
-
-	if(array != NULL){
-		size_t i;
-		for(i = 0; i < size; i++)
-			if(array[i] == value)
-				return (value);
-		return (-1);
-	
-	}
-return (-1);
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	for(i = 0; i < size; i++)
+		if(array[i] == value)
+			return (value);
+	return (-1);
 }
